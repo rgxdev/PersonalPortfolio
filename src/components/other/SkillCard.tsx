d827986f2418b12bc6bf82a80e524f5b8731678d
rgxdev <a.app@gmx.net>
2024-05-23 09:28:56 +0200

    Added funny looking no-copy-paste image

--- src/components/other/SkillCard.tsx
+++ src/components/other/SkillCard.tsx
@@ -4,6 +4,7 @@ import {RiNextjsFill, RiNodejsFill, RiTailwindCssFill} from "react-icons/ri";
 import {motion, useAnimation} from "framer-motion";
 import {useInView} from "react-intersection-observer";
 import {useEffect} from "react";
+import Image from "next/image"; // Adjust the path to your image file
 
 export function SkillCard() {
 
@@ -32,20 +33,34 @@ export function SkillCard() {
             }}
             transition={{duration: 0.5, ease: "easeOut"}}
         >
-            <div className="h-[25rem] flex items-center justify-center w-full">
-                <CardStack items={CARDS}/>
+            <div className="relative h-[25rem] flex items-center justify-center w-full">
+                {/* Unscharfer Hintergrund */}
+                <div className="absolute inset-0 bg-blue-800 w-36 h-32 -left-14 -top-3 blur-xl opacity-80"/>
+
+                {/* Inhalt, der nicht unscharf ist */}
+                <div className="relative z-10 flex items-center justify-center w-full h-full">
+                    <Image
+                        src={"/images/no-copy-paste.png"}
+                        alt="Arrow pointing to the component"
+                        className="absolute -top-3 -rotate-[20deg] -left-14 w-32 h-32"
+                        height={400}
+                        width={400}
+                        draggable={false}
+                    />
+
+                    <CardStack items={CARDS}/>
+                </div>
             </div>
         </motion.div>
+
     );
 }
 
 // Small utility to highlight the content of specific section of a testimonial content
 export const Highlight = ({
                               children,
-                              className,
                           }: {
     children: React.ReactNode;
-    className?: string;
 }) => {
     return (
         <span className="font-bold bg-purple/[0.2] text-purple px-1 py-0.5">
